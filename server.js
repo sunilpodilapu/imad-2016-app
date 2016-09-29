80ff6baee3750b195e03c1811252ddf297a213a0
sunilpodilapu <sunil.p.1729@gmail.com>
2016-09-29 14:24:30 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,7 +8,9 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-
+app.get('/code1',function(req,res){
+  res.sendFile(path.join(__dirname, 'ui', 'code1.html'));
+});
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
