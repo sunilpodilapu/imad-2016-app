7b840299d9a589ffcce2f6d702a1ad06427e241c
sunilpodilapu <sunil.p.1729@gmail.com>
2016-11-20 11:53:29 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -13,8 +13,8 @@ app.get('/code2',function(req,res){
   res.sendFile(path.join(__dirname, 'ui', 'code2.html'));
 });
 
- app.get('/TIC-TAC-TOE',function(req,res){
-  res.sendFile(path.join(__dirname, 'ui', 'TIC-TAC-TOE.html'));
+ app.get('/TIC TAC TOE',function(req,res){
+  res.sendFile(path.join(__dirname, 'ui', 'TIC TAC TOE.html'));
 });
 
  app.get('/TIC-TAC-TOE AI',function(req,res){
